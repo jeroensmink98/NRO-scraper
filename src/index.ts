6da22317da94b5b798f5691c402e1d984d592e68
Jeroen Smink <jeroen@superchat.de>
2022-04-29 09:39:08 +0200

    feat: function to scrape images based on URL given in config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import { Nro } from "./libs/nro";
+import config from "./config";
+
+const nro = new Nro();
+
 async function run() {
+    const data = await nro.getHtml(config.BASE_URL);
+    const images = await nro.parser(data);
     
-} 
+    images.forEach(async (image) => {
+        await nro.downloadFile(image, '../images');
+    });  
+};
 
 run();
